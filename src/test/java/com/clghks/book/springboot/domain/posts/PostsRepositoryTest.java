02175d3143c7c8db84d5a086e5573117fd31b8ca
chihwan <clghks@gmail.com>
2021-01-23 14:41:17 +0900

    코드 정리

--- src/test/java/com/clghks/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/clghks/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.clghks.book.springboot.domain.posts;
 
-import org.apache.tomcat.jni.Local;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 

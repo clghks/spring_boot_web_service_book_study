b31471babbbe93ba734bf125577705527b29d293
chihwan <clghks@gmail.com>
2021-02-20 16:14:15 +0900

    feat: 게시글 등록 및 조회

--- src/main/java/com/clghks/book/springboot/service/PostsService.java
+++ src/main/java/com/clghks/book/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.clghks.book.springboot.service;
 
 import com.clghks.book.springboot.domain.posts.Posts;
 import com.clghks.book.springboot.domain.posts.PostsRepository;
+import com.clghks.book.springboot.web.dto.PostsListResponseDto;
 import com.clghks.book.springboot.web.dto.PostsResponseDto;
 import com.clghks.book.springboot.web.dto.PostsSaveRequestDto;
 import com.clghks.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +34,10 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
